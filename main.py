caa008c986207d46d1cd451fbd4a252d6e19b868
Victor Manalac <115461965+manalacv@users.noreply.github.com>
2025-05-30 15:34:20 +0800

    Add and edit final files

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
-def get_book_text(path_to_file):
-    #1. Takes Filepath as Input
-    with open (path_to_file) as f:
-    #2. Returns Contents of File as a String
-        return f.read()
+import sys
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
+
 
 def main():
-    path="books/frankenstein.txt"
-    text = get_book_text(path)
-    result = num_words(text)
-    print (result)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
 
-def num_words(text):
-    words = text.split()
-    word_count = len(words)
-    return f"{word_count} words found in the document"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
+
 
 main()
-
-
