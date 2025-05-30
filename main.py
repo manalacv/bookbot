55865656d9d6199ebc7b742ae2190adf2f7edef2
Victor Manalac <115461965+manalacv@users.noreply.github.com>
2025-05-30 14:27:33 +0800

    Modify script to print book as string

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+    #1. Takes Filepath as Input
+    with open (path_to_file) as f:
+    #2. Returns Contents of File as a String
+        return f.read()
+
+
+def main():
+    path="books/frankenstein.txt"
+    text = get_book_text(path)
+    print (text)
+
+main()
+
