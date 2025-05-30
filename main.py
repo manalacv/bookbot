f656a998642c2c4e2ad52b35b7f1860e1715867b
Victor Manalac <115461965+manalacv@users.noreply.github.com>
2025-05-30 14:37:41 +0800

    Add stats.py

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ def get_book_text(path_to_file):
     #2. Returns Contents of File as a String
         return f.read()
 
-
 def main():
     path="books/frankenstein.txt"
     text = get_book_text(path)
-    print (text)
+    result = num_words(text)
+    print (result)
+
+def num_words(text):
+    words = text.split()
+    word_count = len(words)
+    return f"{word_count} words found in the document"
 
 main()
+
 
